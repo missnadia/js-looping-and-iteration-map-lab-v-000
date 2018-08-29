0a77ebacfcdd956305bb0e3cde10deb69e5aedc9
Nadia Lee <miss.nadia.lee@gmail.com>
2018-08-29 18:00:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ function lowerCaseDrivers (drivers) {
 
 function nameToAttributes (drivers) {
   return drivers.map(function (driver) {
-    const driverFirst = driver.split(" ")[0];
-    const driverLast = driver.split(" ")[1];
-    return { firstName: driverFirst, lastName: driverLast };
+
+    return { firstName: driver.split(" ")[0], lastName: driver.split(" ")[1] };
   });
 }
